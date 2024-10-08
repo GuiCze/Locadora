075f1f15aa7679b10c54e6376928126968820f28
GuiCze <souguiga@gmail.com>
2024-10-08 19:37:31 -0300

    Second commit

--- src/db.js
+++ src/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`);
-        
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
+          );
+
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
+
     }
 };
 
