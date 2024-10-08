c0bf3c59d1da720550971aa69c44a5494bf27dab
GuiCze <souguiga@gmail.com>
2024-10-08 19:28:41 -0300

    Second commit

--- src/db.js
+++ src/db.js
@@ -7,10 +7,7 @@ const connectDB = async() => {
         );
         
     } catch (error) {
-        console.log(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
-    );
         console.error('Erro ao conectar ao MongoDB', error);
-
     }
 };
 
